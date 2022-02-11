aeb3dc443df77b6b94b42380b5304509fe9c371d
Rohit <prjagtap90@gmail.com>
2022-02-11 22:09:38 +0530

    fe

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -15,7 +15,7 @@ export default function List({ list }) {
             setSlideNumber(slideNumber - 1)
             listRef.current.style.transform = `translateX(${230 + distance}px)`
         }
-        if (direction === "right" && slideNumber < 2) {
+        if (direction === "right" && slideNumber < 2) { /* how many rows u want to scroll ex.2*/
             setSlideNumber(slideNumber + 1)
             listRef.current.style.transform = `translateX(${-230 + distance}px)`
         }

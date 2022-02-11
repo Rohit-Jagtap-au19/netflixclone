bf5aa455273a20e283d043aa2a3254f80d8514a4
Rohit <prjagtap90@gmail.com>
2022-02-11 22:07:38 +0530

    fe

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -15,7 +15,7 @@ export default function List({ list }) {
             setSlideNumber(slideNumber - 1)
             listRef.current.style.transform = `translateX(${230 + distance}px)`
         }
-        if (direction === "right" && slideNumber < 5) {
+        if (direction === "right" && slideNumber < 3) {
             setSlideNumber(slideNumber + 1)
             listRef.current.style.transform = `translateX(${-230 + distance}px)`
         }
